d5504ae1c2acf38334c4f9f964526bbea072fe36
mcordes <mcordes@debian.mcordes>
2025-07-21 11:36:15 +0200

    p2 | apps came

--- p2/scripts/server_1_setup.sh
+++ p2/scripts/server_1_setup.sh
@@ -9,4 +9,6 @@ sudo ufw disable
 echo "alias k='sudo kubectl'" >> /home/vagrant/.bashrc
 source /home/vagrant/.bashrc
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --token 12345" sh -s -
+mkdir /home/vagrant/inception_of_things
+
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode --node-ip=192.168.56.110 --token 12345" sh -s -
